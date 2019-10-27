d6fb4d9b624e34f67e6459e68568c73e711436cc
Flavio Kreis <flaviokreis@gmail.com>
2019-10-27 12:16:42 -0300

    Add help order route and sent answer email

--- src/routes.js
+++ src/routes.js
@@ -6,6 +6,7 @@ import PlanController from './app/controllers/PlanController';
 import RegistrationController from './app/controllers/RegistrationController';
 import CheckinController from './app/controllers/CheckinController';
 import StudentHelpOrderController from './app/controllers/StudentHelpOrderController';
+import HelpOrderController from './app/controllers/HelpOrderController';
 
 import authMiddleware from './app/middlewares/auth';
 
@@ -39,5 +40,8 @@ routes.get('/registrations', RegistrationController.index);
 routes.post('/registrations', RegistrationController.store);
 routes.put('/registrations/:id', RegistrationController.update);
 routes.delete('/registrations/:id', RegistrationController.delete);
+
+routes.post('/help_orders/:id/answer', HelpOrderController.store);
+routes.get('/help_orders', HelpOrderController.index);
 
 export default routes;
